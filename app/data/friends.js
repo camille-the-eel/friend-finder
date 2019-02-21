d12a3501a24838918b2437713faeff327f478b1b
Camille <c.hughes64@gmail.com>
2019-02-21 15:02:11 -0500

    post comparison

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,23 @@
 //DATA
 
-var friendsArray = [];
+var friendsArray = [
+    {
+        friendName: "Vanessa",
+        photo: "vanessa.jpg",
+        survey: [
+            1,
+            4,
+            5,
+            2,
+            3,
+            5,
+            1,
+            4,
+            3,
+            5
+        ]
+    }
+];
 
 //EXPORTING THE ARRAY
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,9 +12,38 @@ module.exports = function(app) {
 
     //POST REQUEST
     //SURVEY INPUT IS SUBMITTED TO THE SERVER AS A JSON OBJECT
-    //JSON OBJECT IS PUSHED TO JS ARRAY, SERVER SAVES DATA TO THE SPECIFIED (FRIENDSDATA) ARRAY
     app.post("/api/friends", function(req, res) {
+
+        //JSON OBJECT IS PUSHED TO JS ARRAY, SERVER SAVES DATA TO THE SPECIFIED (FRIENDSDATA) ARRAY
         friendsData.push(req.body);
-        res.json(true);
+
+        //THE LAST SUBMIT IS SET AS THE NEWEST USER (AWAITING COMPARISON)
+        var newFriendData = req.body.survey;
+        console.log("data" + friendsData);
+        console.log("new" + newFriendData);
+
+        var eachFriendsTotal = 0;
+        var friendsTotalArr = [];
+
+        //
+        for (var i = 0; i < friendsData.length -1; i++) {
+            eachFriendsTotal = 0;
+            for (var j = 0; j < 10; j++) {
+                eachFriendsTotal += (Math.abs(parseInt(friendsData[i].survey[j]) - parseInt(newFriendData[j])))
+                console.log("Each" + eachFriendsTotal);
+            }
+            friendsTotalArr.push(eachFriendsTotal);
+            console.log("Arr" + friendsTotalArr);
+        };
+        
+        //SORTING THE TOTAL DIFFERENCES OF THE FRIENDS INTO ASCENDING ORDER (LOWEST DIFFERENCE AT INDEX 0)
+        var sortedFriendMatches = friendsTotalArr.sort(function(a, b){return a - b});
+        console.log("sort" + sortedFriendMatches);
+        var bestMatch = sortedFriendMatches[0];
+
+        console.log(bestMatch);
+
+        res.json(bestMatch);
+
     });
 }
